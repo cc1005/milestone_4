fb62c12cdd71fdd716e75d8aba16764488e8bf31
Conor Campbell <conorcampbell@live.com>
2020-05-01 23:06:30 +0100

    Cart functionality added and search functionality currently being worked on

--- archivelibrary/settings.py
+++ archivelibrary/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'accounts',
     'products',
     'home',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents'
             ],
         },
     },
