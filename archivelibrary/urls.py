1091c9d5b21b5c934910c8bbdc0b2d376a2b91b0
Conor Campbell <conorcampbell@live.com>
2020-05-03 23:09:15 +0100

    two additional unnecessary apps deleting - Home and Cart

--- archivelibrary/urls.py
+++ archivelibrary/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as urls_accounts
-from cart import urls as urls_cart
 from catalogue.views import total_catalogue
 from search import urls as urls_search
 from checkout import urls as urls_checkout
@@ -28,7 +27,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', total_catalogue, name='index'),
     url(r'^accounts/', include(urls_accounts)),
-    url(r'^cart/', include(urls_cart)),
     url(r'^search/', include(urls_search)),
     url(r'^checkout/', include(urls_checkout)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),  
--- home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    """This view displays the index page"""
-    return render(request, "index.html")
