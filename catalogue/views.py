57a176aac9af39dc52913281fb63a5871658bcc3
Conor Campbell <conorcampbell@live.com>
2020-05-05 00:14:19 +0100

    significant updates to app design through bootstrap, continued efforts to resolve difficulties iterating over full documents still not resulted in a fix.

--- catalogue/views.py
+++ catalogue/views.py
@@ -12,12 +12,14 @@ def total_catalogue(request):
 
 def access_full_documents(request):
     fulldocument = get_object_or_404(FullDocument)
+    catalogue = get_object_or_404(Catalogue)
     if request.user.is_authenticated:
         try:
             if request.user:
                 customer = Customer.objects.get(user=request.user)
-                return render(request, "fulldocument.html", {'fulldocument':fulldocument, 'customer':customer})
+                return render(request, "fulldocument.html", {'fulldocument':fulldocument, 'customer':customer, 'catalogue':catalogue})
         except Customer.DoesNotExist:
             return redirect('registration')
     else:
         return redirect('registration')
+
