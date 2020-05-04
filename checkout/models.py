da92c99695e3d4eb12a097b9c2d926229c54f86b
Conor Campbell <conorcampbell@live.com>
2020-05-04 16:58:10 +0100

    Plans app removed and updated bootstrap of base.html significantly (but still with significant work to do)

--- checkout/models.py
+++ checkout/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Customer(models.Model):
-    user = models.ForeignKey(User, related_name='premium_plan_user', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name='premium_plan_user', on_delete=models.CASCADE)
     stripeid = models.CharField(max_length=255)
     stripe_subscription_id = models.CharField(max_length=255)
     cancel_at_period_end = models.BooleanField(default=False)
--- testing.py
+++
@@ -1,6 +0,0 @@
-import os
-from os import path
-if path.exists("env.py"):
-  import env 
-
-print(os.getenv("TESTVAR"))
