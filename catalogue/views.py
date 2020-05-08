ae191e400cfe1ac4507f375fb062a8298c39536d
Conor Campbell <conorcampbell@live.com>
2020-05-08 19:49:44 +0100

    Finally have the central loop working - only those with premium access can access the full files which are obtained through the link on the catalogue.html page. I have some concerns about the allocation of IDs to the full records however - the next step is to make sure that both the catalogue and full record share the same ID, or at least to figure out some way to ensure the full record produced is always the correct one.

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Catalogue, FullDocument
+from django.contrib import messages
 from django.contrib.auth import authenticate
 from checkout.models import Customer
 from checkout.views import checkout
@@ -10,9 +11,10 @@ def total_catalogue(request):
     catalogue = Catalogue.objects.all()
     return render(request, "catalogue.html", {"catalogue": catalogue})
 
-def access_full_documents(request, fulldocument_id):
-    fulldocument = get_object_or_404(FullDocument, pk=fulldocument_id)
-    catalogue = get_object_or_404(Catalogue)
+"""
+def access_full_documents(request, catalogue_id):
+    fulldocument = get_object_or_404(FullDocument)
+    catalogue = get_object_or_404(Catalogue, )
     if request.user.is_authenticated:
         try:
             if request.user:
@@ -22,5 +24,31 @@ def access_full_documents(request, fulldocument_id):
             return redirect('registration')
     else:
         return redirect('registration')
+"""
+
+def access_full_documents(request, catalogue_id):
+    catalogue = get_object_or_404(Catalogue, pk=catalogue_id)
+    fulldocument = get_object_or_404(FullDocument, pk=catalogue_id)
+    title = catalogue.name
+    description = catalogue.description
+    fulltext = fulldocument.document_text
+    context = {
+        "catalogue": catalogue,
+        "title": title,
+        "description": description,
+        "fulltext": fulltext,
+    }
+    if request.user.is_authenticated:
+        try:
+            if request.user:
+                customer = Customer.objects.get(user=request.user)
+                return render(request, "fulldocument.html", context)
+        except Customer.DoesNotExist:
+            messages.error(request, "You must have premium membership to access this document")
+            return redirect('registration')
+    else:
+        return redirect('registration')
+
+    
 
 
