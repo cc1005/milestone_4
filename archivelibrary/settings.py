39b6d9997f278cb59b17c36bacb4f8bb7c82b41a
Conor Campbell <conorcampbell@live.com>
2020-05-10 00:20:54 +0100

    Heroku added to allowed hosts

--- archivelibrary/settings.py
+++ archivelibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DEFAULTSECRETKEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://finalmilestoneproject.herokuapp.com/']
 
 # Application definition
 
